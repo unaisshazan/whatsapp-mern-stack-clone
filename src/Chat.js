3db1e202ea51025d820f9810e0401534b3a8c7d4
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-17 23:31:11 +0500

    Update Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,5 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, Icon, IconButton } from '@material-ui/core'
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
 import React from 'react'
 import './Chat.css'
 function Chat() {
@@ -9,6 +10,18 @@ function Chat() {
               </div>
               <div className="chat__headerInfo">
                   <h3>Room Name</h3>
+                  <p>LAST SEEN</p>
+              </div>
+              <div className="chat__headerRight">
+ <IconButton>
+     <SearchOutlined/>
+ </IconButton>
+ <IconButton>
+     <AttachFile />
+ </IconButton>
+ <IconButton>
+     <MoreVert />
+ </IconButton>
               </div>
        
         </div>
