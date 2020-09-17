d26575dbd9a03521120b38f53aed35068dd53c14
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-17 22:19:02 +0500

    Update Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import './Sidebar.css'
 import ChatIcon from '@material-ui/icons/Chat';
-import { DonutLargeIcon } from '@material-ui/icons';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';import { IconButton } from '@material-ui/core';
 function Sidebar() {
     return (
         <div className="sidebar">
 
             <div className="sidebar__header">
                 <div className="sidebar__headerRight">
-<IconButton></IconButton>
+<IconButton>
 <DonutLargeIcon />
-                </div>
+</IconButton>
+    </div>
             </div>
         </div>
     )
