03c95d28f0e10aaea4d43a9f79191a32c0694538
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-17 22:36:40 +0500

    Update Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Sidebar.css'
 import ChatIcon from '@material-ui/icons/Chat';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';import { Avatar, IconButton } from '@material-ui/core';
-import { MoreVert } from '@material-ui/icons';
+import { MoreVert, SearchOutlined } from '@material-ui/icons';
 function Sidebar() {
     return (
         <div className="sidebar">
@@ -20,6 +20,12 @@ function Sidebar() {
     <MoreVert/>
 </IconButton>
     </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder="Search or Start a new chat" type="text"/>
+                </div>
             </div>
         </div>
     )
