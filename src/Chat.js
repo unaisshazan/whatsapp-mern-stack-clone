3c880a6626a646fc2cd702f8e245c5dda4c7a0ae
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-18 00:03:16 +0500

    Update Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -1,31 +1,41 @@
-import { Avatar, Icon, IconButton } from '@material-ui/core'
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
-import React from 'react'
-import './Chat.css'
+import { Avatar, Icon, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import React from "react";
+import "./Chat.css";
 function Chat() {
-    return (
-        <div className='chat'>
-              <div className="chat__header">
-                  <Avatar/>
-            
-              <div className="chat__headerInfo">
-                  <h3>Room Name</h3>
-                  <p>LAST SEEN</p>
-              </div>
-              <div className="chat__headerRight">
- <IconButton>
-     <SearchOutlined/>
- </IconButton>
- <IconButton>
-     <AttachFile />
- </IconButton>
- <IconButton>
-     <MoreVert />
- </IconButton>
-              </div>
-       
-        </div>  </div>
-    )
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>LAST SEEN</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+          <p> <span className="chat__name">unais</span>
+              This is a message
+              <span className="chat__timestamp">
+                  {new Date().toUTCString()}
+              </span>
+              
+              </p>
+
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
