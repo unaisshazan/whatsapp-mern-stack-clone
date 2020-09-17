117e70f4483c8b9c54070608b1370949dc7973d9
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-17 14:28:05 +0500

    add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="app">
       <div className="app__body">
-        <h1>Whatsapp mern</h1>
+       
         <Sidebar />
         <Chat />
       </div>
