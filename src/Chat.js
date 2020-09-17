173311553cff885a5ac8cd5558e5c8f89b923ffe
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-17 14:12:59 +0500

    add

---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css'
+function Chat() {
+    return (
+        <div className='chat'>
+           <h2>chat</h2> 
+        </div>
+    )
+}
+
+export default Chat
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import './Sidebar.css'
 function Sidebar() {
     return (
-        <div>
-            
+        <div className="sidebar">
+
+            <h1>sidebar</h1>
         </div>
     )
 }
