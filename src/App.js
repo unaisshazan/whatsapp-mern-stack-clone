33d38ff41901e630340c4fa95fc120f2ec855f24
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-17 13:56:48 +0500

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+  <h1>Whatsapp mern</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDrsdr83jeZXCRE1t7IAVLpGg2fAVzO8QM",
+    authDomain: "whatsapp-clone-mernbuild.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-mernbuild.firebaseio.com",
+    projectId: "whatsapp-clone-mernbuild",
+    storageBucket: "whatsapp-clone-mernbuild.appspot.com",
+    messagingSenderId: "340021706123",
+    appId: "1:340021706123:web:06f4376f2ac128538f438e",
+    measurementId: "G-8BEK7F8DTW"
+  };
